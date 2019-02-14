e2729d0d1628c3fe9c355dab5844999ee5fa1f01
Dhanasekar <dhanasekar.rohith@gmail.com>
2019-02-14 18:36:17 +0530

    Validation completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import InputField from "./components/inputField";
 import RadioButton from "./components/radioButton";
 import TextArea from "./components/textArea";
 import TableClass from "./components/table";
-import CheckBox from "./components/checkBox"
+import CheckBox from "./components/checkBox";
 import "./App.css";
 
 class App extends Component {
@@ -22,26 +22,35 @@ class App extends Component {
       commAdd: "",
       perAdd: ""
     },
+    errorMsgs: {
+      firstName: "",
+      lastName: "",
+      dob: "",
+      email: "",
+      phone: "",
+      commAdd: "",
+      perAdd: ""
+    },
     row: []
   };
   checkedValue = key => {
     if (key === "gender") {
       this.setGender();
     } else if (key === "perAdd") {
-      let sameAdd = this.state.sameAdd;
+      let sameAdd = !this.state.sameAdd;
       this.setState({
-        sameAdd: !sameAdd
-      })
+        sameAdd
+      });
       let userDetails = this.state.userDetails;
-      if(sameAdd){   
+      if (sameAdd) {
         userDetails.perAdd = this.state.userDetails.commAdd;
-        console.log(userDetails.perAdd) 
+        console.log(userDetails.perAdd);
       } else {
         userDetails.perAdd = "";
       }
       this.setState({
         userDetails
-      })
+      });
     }
   };
 
@@ -66,6 +75,16 @@ class App extends Component {
       row: rows
     });
     this.setGender();
+    // userDetails:{
+    //   firstName: "",
+    //   lastName: "",
+    //   dob: "",
+    //   gender: "Male",
+    //   email: "",
+    //   phone: "",
+    //   commAdd: "",
+    //   perAdd: ""
+    // }
     this.setState({
       userDetails: {
         firstName: "",
@@ -78,6 +97,26 @@ class App extends Component {
         perAdd: ""
       }
     });
+  };
+
+  validation = () => {
+    let userDetails = this.state.userDetails;
+    let errorMsgs = this.state.errorMsgs;
+    let check = false;
+    Object.keys(userDetails).map((key)=> {
+     switch(key){
+       case key : errorMsgs[key] = userDetails[key] === "" ? ("*"+key+" field can't be blank") : "";
+     }
+     if(errorMsgs[key]!="")
+      check=true;
+    console.log(errorMsgs)
+    });
+    
+    this.setState({
+      errorMsgs
+    });
+    if(check===false)
+      this.createOnClick();
   };
 
   inputFieldOnChange = (key, event) => {
@@ -98,6 +137,9 @@ class App extends Component {
               onChange={event => this.inputFieldOnChange("firstName", event)}
               value={this.state.userDetails.firstName}
             />
+            <div className="error-msg">
+              {this.state.errorMsgs.firstName}
+            </div>
           </div>
           <div className="grid-items">
             LastName
@@ -107,6 +149,9 @@ class App extends Component {
               onChange={event => this.inputFieldOnChange("lastName", event)}
               value={this.state.userDetails.lastName}
             />
+            <div className="error-msg">
+              {this.state.errorMsgs.lastName}
+            </div>
           </div>
           <div className="grid-items">
             DOB
@@ -116,6 +161,7 @@ class App extends Component {
               onChange={event => this.inputFieldOnChange("dob", event)}
               value={this.state.userDetails.dob}
             />
+            <div className="error-msg">{this.state.errorMsgs.dob}</div>
           </div>
           <div className="grid-items">
             Gender
@@ -141,6 +187,7 @@ class App extends Component {
               onChange={event => this.inputFieldOnChange("email", event)}
               value={this.state.userDetails.email}
             />
+            <div className="error-msg">{this.state.errorMsgs.email}</div>
           </div>
           <div className="grid-items">
             Phone <br />
@@ -148,6 +195,7 @@ class App extends Component {
               onChange={event => this.inputFieldOnChange("phone", event)}
               value={this.state.userDetails.phone}
             />
+            <div className="error-msg">{this.state.errorMsgs.phone}</div>
           </div>
           <div className="grid-items">
             Communication Address
@@ -156,6 +204,7 @@ class App extends Component {
               onChange={event => this.inputFieldOnChange("commAdd", event)}
               value={this.state.userDetails.commAdd}
             />
+            <div className="error-msg">{this.state.errorMsgs.commAdd}</div>
             <div>
               <CheckBox
                 value={"perAdd"}
@@ -171,10 +220,11 @@ class App extends Component {
               onChange={event => this.inputFieldOnChange("perAdd", event)}
               value={this.state.userDetails.perAdd}
             />
+            <div className="error-msg">{this.state.errorMsgs.perAdd}</div>
           </div>
         </div>
         <div className="create-div">
-          <div className="create-button" onClick={() => this.createOnClick()}>
+          <div className="create-button" onClick={() => this.validation()}>
             Create
           </div>
         </div>
